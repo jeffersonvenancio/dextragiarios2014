95ac29aaebcaff2eebc62a2c443d4bf397593624
jeffersonvenancio <jefferson.venancio@dextra-sw.com>
2013-12-27 11:05:57 -0200

    Primeira Atividade

--- desafio-buzz/src/main/java/com/dextra/dextragiarios2014/atividade1/Atividade1.java
+++ desafio-buzz/src/main/java/com/dextra/dextragiarios2014/atividade1/Atividade1.java
@@ -1,5 +1,18 @@
 package com.dextra.dextragiarios2014.atividade1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Atividade1 {
+
+	public List<String> encontrarPessoaPorNome(Map<Long, String> pessoas, String nome) {
+		List<String> pessoasEncontradas = new ArrayList<String>();
+		for (int i = 0; i < pessoas.size(); i++) {
+			if (pessoas.get(i) == nome)
+				pessoasEncontradas.add(nome);
+		}
+		return pessoasEncontradas;
+	}
 
 }
--- desafio-buzz/src/test/java/com/dextra/dextragiarios2014/test/atividade1/Atividade1Test.java
+++ desafio-buzz/src/test/java/com/dextra/dextragiarios2014/test/atividade1/Atividade1Test.java
@@ -1,5 +1,28 @@
 package com.dextra.dextragiarios2014.test.atividade1;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.dextra.dextragiarios2014.atividade1.Atividade1;
+
 public class Atividade1Test {
 
+	@Test
+	public void recuperaMarias() {
+
+		Map<Long, String> pessoas = new HashMap<Long, String>();
+
+		pessoas.put(1l, "Maria");
+		pessoas.put(2l, "Jose");
+		pessoas.put(3l, "Antonio");
+		pessoas.put(4l, "Tiago");
+
+		List<String> pessoasEncontradas = new Atividade1().encontrarPessoaPorNome(pessoas, "Maria");
+
+		Assert.assertEquals(1, pessoasEncontradas.size());
+	}
 }
