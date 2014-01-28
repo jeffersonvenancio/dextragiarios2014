34231dd9017ceea75e1e110b0c8ce35fdfdd57e7
Leonardo Couto <leonardo.couto@dextra-sw.com>
2014-01-28 16:56:43 -0200

    Tentativa de colocar o JPA

--- webapp/src/main/java/com/dextra/webapp/entity/Usuario.java
+++ webapp/src/main/java/com/dextra/webapp/entity/Usuario.java
@@ -1,10 +1,27 @@
 package com.dextra.webapp.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	
+	@Column(length = 250)
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
 }
