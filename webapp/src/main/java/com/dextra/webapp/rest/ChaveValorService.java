6b409ad9739dd63a848b676367d9bc4e2299a055
Leonardo Couto <leonardo.couto@dextra-sw.com>
2014-02-05 16:59:00 -0200

    Servico de chave-valor no webapp, inclusão de dados e exibição da lista de chaves-valor

--- webapp/src/main/java/com/dextra/webapp/rest/ChaveValorService.java
+++ webapp/src/main/java/com/dextra/webapp/rest/ChaveValorService.java
@@ -1,47 +1,85 @@
 package com.dextra.webapp.rest;
 
 import java.security.Principal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
-import javax.annotation.Resource;
-import javax.ejb.SessionContext;
-import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 import com.dextra.webapp.entity.ChaveValor;
 import com.dextra.webapp.vo.ChaveValorVO;
 
-@Stateless
 @Path("chave-valor")
 public class ChaveValorService {
+	
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
-    @Resource
-    SessionContext context;
-    
     @PersistenceContext
     private EntityManager em;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("vo")
-    public ChaveValorVO page() {
+    public ChaveValorVO page(@Context HttpServletRequest request) {
         
-        Principal principal = context.getCallerPrincipal();
+        Principal principal = request.getUserPrincipal();
         String name = principal.getName();
-
-        String jpql = String.format("SELECT c"
-                + " FROM %s c"
-                + " WHERE c.usuario = :usuario", ChaveValor.class.getSimpleName());
-        TypedQuery<ChaveValor> query = this.em.createQuery(jpql, ChaveValor.class);
-        List<ChaveValor> values = query.getResultList();
+        List<ChaveValor> values = listar(name);
         
         return new ChaveValorVO(name, values);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("")
+    public List<ChaveValor> listar(@Context HttpServletRequest request) {
+
+    	Principal principal = request.getUserPrincipal();
+        String name = principal.getName();
+    	return listar(name);
+    }
+    
+    private List<ChaveValor> listar(String usuario) {
+    	String jpql = String.format("SELECT c"
+                + " FROM %s c"
+                + " WHERE c.usuario = :usuario", ChaveValor.class.getSimpleName());
+       TypedQuery<ChaveValor> query = this.em.createQuery(jpql, ChaveValor.class);
+       query.setParameter("usuario", usuario);
+       return query.getResultList();
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("") 
+    public ChaveValor novo(
+    		@FormParam("chave") String chave,
+    		@FormParam("valor") String valor,
+    		@Context HttpServletRequest request) {
+    	
+    	Principal principal = request.getUserPrincipal();
+    	String usuario = principal.getName();
+    	String data = DATE_FORMAT.format(Calendar.getInstance().getTime());
+    	
+    	ChaveValor chaveValor = new ChaveValor();
+    	chaveValor.setChave(chave);
+    	chaveValor.setDataHora(data);
+    	chaveValor.setUsuario(usuario);
+    	chaveValor.setValor(valor);
+    	
+    	this.em.persist(chaveValor);
+    	
+    	return chaveValor;
     }
 
 }
--- webapp/src/main/webapp/js/chave-valor.js
+++ webapp/src/main/webapp/js/chave-valor.js
@@ -8,9 +8,35 @@ Dextra = (function($, handle) {
 				var template = Handlebars.compile(source);
 
 				$.getJSON("/api/chave-valor/vo", {}, function(vo) {
-					//$("#main-chave-valor").html(template(vo.value));
+
+					$("#main-chave-valor").html(template(vo.value));
+					Dextra.ChaveValor.atualizaTabela(vo.value.valores);
+					
+					$("#submeter").click(function() {
+						var chave = $("#chave").val();
+						var valor = $("#valor").val();
+						
+						$.post("/api/chave-valor", {
+							"chave" : chave,
+							"valor" : valor
+						}, function(chaveValor) {
+							Dextra.ChaveValor.carregaTabela();
+						});
+					});
 				});  
-			} 
+			},
+			
+			"carregaTabela" : function() {
+				$.getJSON("/api/chave-valor", {}, function(listar) {
+					Dextra.ChaveValor.atualizaTabela(listar.chaveValor);
+				});
+			},
+			
+			"atualizaTabela" : function(valores) {
+				var source = $("#template-tabela-chave-valor").html();
+				var template = Handlebars.compile(source);
+				$("#tabela-valores").html(template({"valores" : valores}));
+			}
 			
 	};
 
